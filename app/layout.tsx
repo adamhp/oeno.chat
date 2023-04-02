6830c1b06cd60e845f90b4d6ff697c8da7c71f3e
Adam Pearce <adamp319@gmail.com>
2023-04-02 14:51:39 -0400

    Update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,50 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Wine Pairings',
-  description: '',
+  title: 'oeno.chat - Your AI-powered sommelier',
+  description:
+    'oeno.chat is an AI-powered sommelier that helps you find the perfect wine to pair with your food.',
+  openGraph: {
+    title: 'oeno.chat - Your AI-powered sommelier',
+    description:
+      'oeno.chat is an AI-powered sommelier that helps you find the perfect wine to pair with your food.',
+    url: 'https://oeno.chat',
+    siteName: 'oeno.chat',
+    images: [
+      {
+        url: 'https://oeno.chat/social.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  applicationName: 'oeno.chat',
+  keywords: [
+    'Next.js',
+    'React',
+    'JavaScript',
+    'TypeScript',
+    'Wine',
+    'ChatGPT',
+    'OpenAI',
+    'GPT-3',
+    'Sommelier',
+    'AI',
+    'Artificial Intelligence',
+    'Machine Learning',
+    'Wine Pairing',
+    'Wine Recommendations',
+    'Food & Wine',
+  ],
+  authors: [
+    { name: 'Adam Pearce', url: 'https://adamhp.io' },
+    { name: 'Mike Danello' },
+  ],
+  colorScheme: 'dark',
+  creator: 'Adam Pearce & Mike Danello',
+  publisher: 'Adam Pearce & Mike Danello',
 };
 
 export default function RootLayout({
