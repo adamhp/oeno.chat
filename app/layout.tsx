826794adc4f896077a8fd87247dcb84430f649ab
Adam Pearce <adamp319@gmail.com>
2023-04-04 09:28:46 -0400

    Fix gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,17 +63,17 @@ export default function RootLayout({
       {env == 'production' && (
         <>
           <Script
-            src='https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID'
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
             strategy='afterInteractive'
           />
           <Script id='google-analytics' strategy='afterInteractive'>
             {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `}
           </Script>
         </>
       )}
